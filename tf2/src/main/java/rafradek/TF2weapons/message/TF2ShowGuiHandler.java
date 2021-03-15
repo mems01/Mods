5c10ec3bb97cb868b0ddc49447694fe22b194844
memsss <aggelos.bilai73@gmail.com>
2021-03-15 15:55:11 +0200

    fix3

--- tf2/src/main/java/rafradek/TF2weapons/message/TF2ShowGuiHandler.java
+++ tf2/src/main/java/rafradek/TF2weapons/message/TF2ShowGuiHandler.java
@@ -1,6 +1,7 @@
 package rafradek.TF2weapons.message;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.entity.Entity;
 import net.minecraft.nbt.NBTTagList;
 import net.minecraftforge.fml.common.network.internal.FMLNetworkHandler;
 import net.minecraftforge.fml.common.network.simpleimpl.IMessage;
@@ -12,32 +13,34 @@ import rafradek.TF2weapons.client.gui.GuiTeamSelect;
 import rafradek.TF2weapons.message.TF2Message.ShowGuiMessage;
 import rafradek.TF2weapons.util.TF2Util;
 
+import java.util.HashMap;
 import java.util.List;
 
 public class TF2ShowGuiHandler implements IMessageHandler<TF2Message.ShowGuiMessage, IMessage> {
 
-	@Override
-	public IMessage onMessage(final ShowGuiMessage message, MessageContext ctx) {
-		if (ctx.side == Side.SERVER) {
-			if (message.id != 99)
-				FMLNetworkHandler.openGui(ctx.getServerHandler().player, TF2weapons.instance, message.id,
-						ctx.getServerHandler().player.world, 0, 0, 0);
-		}
-		else {
-			if (message.id == 100) {
-				NBTTagList listTeams = message.data.getTagList("Teams", 8);
-				List<String> teams = TF2Util.NBTTagListToList(listTeams, String.class);
-				int[] counts = message.data.getIntArray("Count");
-				boolean[] allowed = new boolean[counts.length];
-				byte[] allowedbyte = message.data.getByteArray("Allowed");
-				for(int i = 0; i < allowed.length; i++) {
-					allowed[i] = allowedbyte[i] == 1;
-				}
-				Minecraft.getMinecraft().addScheduledTask(()->Minecraft.getMinecraft().displayGuiScreen(new GuiTeamSelect(teams, counts, allowed)));
-				
-			}
-		}
-		return null;
-	}
+    public static HashMap<Entity, float[]> shotInfo = new HashMap<Entity, float[]>();
+
+    @Override
+    public IMessage onMessage(final ShowGuiMessage message, MessageContext ctx) {
+        if (ctx.side.isServer()) {
+            if (message.id != 99)
+                FMLNetworkHandler.openGui(ctx.getServerHandler().player, TF2weapons.instance, message.id,
+                        ctx.getServerHandler().player.world, 0, 0, 0);
+        } else {
+            if (message.id == 100) {
+                NBTTagList listTeams = message.data.getTagList("Teams", 8);
+                List<String> teams = TF2Util.NBTTagListToList(listTeams, String.class);
+                int[] counts = message.data.getIntArray("Count");
+                boolean[] allowed = new boolean[counts.length];
+                byte[] allowedbyte = message.data.getByteArray("Allowed");
+                for (int i = 0; i < allowed.length; i++) {
+                    allowed[i] = allowedbyte[i] == 1;
+                }
+                Minecraft.getMinecraft().addScheduledTask(() -> Minecraft.getMinecraft().displayGuiScreen(new GuiTeamSelect(teams, counts, allowed)));
+
+            }
+        }
+        return null;
+    }
 
 }
